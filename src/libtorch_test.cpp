ddfbba9684113d0d50bf7fed6be2370d16a7a4f8
Shuhei-YOSHIDA <yoshida0525shu@gmail.com>
2024-09-02 01:20:48 +0900

    added libtorch_test as minimal executable

--- src/libtorch_test.cpp
+++ src/libtorch_test.cpp
@@ -1,9 +1,12 @@
 /**
  * @file libtorch_test.cpp
+ * @note sample for minimum executable for libtorch
  */
 
-int main(int argc, char **argv)
-{
-  
-  return 0;
+#include <torch/torch.h>
+#include <iostream>
+
+int main() {
+  torch::Tensor tensor = torch::rand({2, 3});
+  std::cout << tensor << std::endl;
 }
